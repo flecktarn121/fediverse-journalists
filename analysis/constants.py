dd3e5dddea4c916df3f7435ac0c34438c500d828
Ángel García Menéndez <UO258654@uniovi.es>
2023-07-21 12:15:36 +0200

    Small bug fix in the reading of the misspells file

--- analysis/constants.py
+++ analysis/constants.py
@@ -13,7 +13,7 @@ HEDOMETER_LEXICON = 'analysis/data/hedometer/polarizing_words.csv'
 COMMON_WORDS = 'analysis/data/hedometer/count_1w.txt'
 MISSPELLINGS_FILE = 'analysis/resources/Test_Set_3802_Pairs.txt'
 EMOTICONS_FILE = 'analysis/resources/wikipedia_emoticons.tsv'
-SPACY_MODEL = 'en_core_web_lg'
+SPACY_MODEL = 'en_core_web_sm'
 WIKIDATA_API_URL = 'https://www.wikidata.org/w/api.php?action=wbsearchentities&language=en&format=json&search='
 WIKIDATA_GET_API = 'https://www.wikidata.org/w/api.php?action=wbgetentities&sites=enwiki&languages=en&format=json&ids='
 NUM_CLUSTERS = 150
--- analysis/utils/split.py
+++ analysis/utils/split.py
@@ -3,10 +3,11 @@ import ijson
 import json
 
 def split():
-    filenames = os.listdir('data/raw/mastodon')
+    directory = 'analysis/data/raw/mastodon/raw'
+    filenames = os.listdir(directory)
     for filename in filenames:
         if filename.endswith('.json'):
-            with open(f'data/raw/mastodon/{filename}', 'r') as f:
+            with open(f'{directory}/{filename}', 'r') as f:
                 counter = 0
                 filename_counter = 0
                 posts = []
@@ -14,12 +15,12 @@ def split():
                     posts.append(post)
                     counter += 1
                     if counter == 1000:
-                        with open(f'data/split/mastodon/{filename}{filename_counter}.json', 'w') as outfile:
+                        with open(f'analysis/data/raw/mastodon/{filename}{filename_counter}.json', 'w') as outfile:
                             json.dump(posts, outfile, indent=4, default=str)
                         counter = 0
                         posts = []
                         filename_counter += 1
-                with open(f'data/split/mastodon/{filename}{filename_counter}.json', 'w') as outfile:
+                with open(f'analysis/data/raw/mastodon/{filename}{filename_counter}.json', 'w') as outfile:
                     json.dump(posts, outfile, indent=4, default=str)
 
 
